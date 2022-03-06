796c1018000c64e8c204f4e2d64ef7d6bc0e7f48
muckypops <muckypops@gmail.com>
2022-03-05 21:51:22 -0600

    modified

--- ncrypt.c
+++ ncrypt.c
@@ -1,3 +1,11 @@
+/*
+ncrypt is a simple symmetric key file encryption utility.
+
+usage: 
+    encrypt:    cat infilename | ./ncrypt -k <key> > encryptedfilename
+    decrypt:    cat encryptedfilename | ./ncrypt -k <key> -d outfilename
+*/
+
 #include "lib/encryption.h"
 #include <stdio.h>
 #include <unistd.h>
