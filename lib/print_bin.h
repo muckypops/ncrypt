904bdc51357014f28890f239e51be572203509fe
muckypops <muckypops@gmail.com>
2022-03-05 21:55:35 -0600

    removed unneeded files

--- lib/print_bin.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void print_bin(unsigned int integer, unsigned int bitcount) {
-    while(bitcount--) {
-        printf("%d", ((integer >> bitcount) & 1));
-    }
-}
--- lib/printhex.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void printhex(unsigned char *msg, int msglen) {
-    for(int i = 0; i < msglen; i++) 
-        printf("0x%02X ", msg[i]);
-    putchar('\n');
-}
