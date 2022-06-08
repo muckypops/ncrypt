e9ec0429226c24d463283032a79adb299339e384
Miguel DeCastro <muckypops@gmail.com>
2022-06-08 08:57:35 -0500

    changes

--- cpp/cryptor.cpp
+++ cpp/cryptor.cpp
@@ -1,2 +1,42 @@
 #include "cryptor.h"
 
+int rotatebits(const uint8_t inchar, const int offset, const int lorr) {
+    int tmpchar;
+
+    switch (lorr) {
+        case RB_LEFT:  tmpchar = ((inchar << offset) | (inchar >> (CHAR_BIT - offset))); break;
+        case RB_RIGHT: tmpchar = ((inchar >> offset) | (inchar << (CHAR_BIT - offset))); break;
+        default:       tmpchar = inchar;
+    }
+
+    return tmpchar;
+}
+
+int flipcrypt(FILE *fpin, FILE *fpout, const char *key, const int eord) {
+
+    int tmpchar;
+    int keylen = strlen(key);
+    int ik, broffset;
+
+    ik = 0;
+
+    tmpchar = fgetc(fpin);
+    while (tmpchar != EOF) {
+        
+        // broffset determines bit rotation offset based on key value
+        broffset = (key[ik] % CHAR_BIT);
+
+        if(eord == FC_ENCRYPT) tmpchar = rotatebits(tmpchar, broffset, RB_RIGHT);
+
+        tmpchar = ((~tmpchar) ^ key[ik]);
+
+        if(eord == FC_DECRYPT) tmpchar = rotatebits(tmpchar, broffset, RB_LEFT);
+
+        if(++ik == (keylen)) ik = 0;
+        
+        fputc(tmpchar, fpout);
+        tmpchar = fgetc(fpin);
+    }
+
+    return 0;
+}
--- cpp/cryptor.h
+++ cpp/cryptor.h
@@ -1,6 +1,11 @@
 #ifndef _CRYPTOR_H_
 #define _CRYPTOR_H_
 
+#define FC_ENCRYPT 1
+#define FC_DECRYPT 0
+
+#define RB_LEFT 1
+#define RB_RIGHT 0
 
 
 
